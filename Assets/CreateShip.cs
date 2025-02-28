a4ac1dca97a4e94de0ac3e538ad8b6b1a2b84b67
YusufDurak <yunturneddrk@gmail.com>
2025-02-28 22:08:27 +0300

    CreateShip

--- Assets/CreateShip.cs
+++ Assets/CreateShip.cs
@@ -1,6 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
-public class CreateShip
+public class ObjectSpawner : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject objectPrefab; // Prefab to spawn
+    [SerializeField] private Transform spawnPoint; // Spawn position
+    [SerializeField] private float spawnInterval = 5f; // Time interval
+
+    private void Start()
+    {
+        StartCoroutine(SpawnObjects());
+    }
+
+    private IEnumerator SpawnObjects()
+    {
+        while (true)
+        {
+            SpawnObject();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private void SpawnObject()
+    {
+        if (objectPrefab != null)
+        {
+            Instantiate(objectPrefab, spawnPoint.position, Quaternion.identity);
+        }
+    }
 }
