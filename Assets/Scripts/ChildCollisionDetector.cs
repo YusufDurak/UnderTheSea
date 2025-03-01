abfe8afbea3895fd339f758600a594db1372f39d
YusufDurak <yunturneddrk@gmail.com>
2025-03-01 11:17:26 +0300

    BigPush

---
+++ Assets/Scripts/ChildCollisionDetector.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ChildCollisionDetector : MonoBehaviour
+{
+    public static int hit = 0;
+    private float lastCountTime = 0f;
+    private bool isColliding = false; // Tracks if currently colliding
+    [SerializeField] private float countingCooldown = 0.2f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("whale") && !isColliding) // 
+        {
+            isColliding = true; // Collision started
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("whale") && isColliding && Time.time >= lastCountTime + countingCooldown)
+        {
+
+            
+            hit++; 
+            Debug.Log("Exited collision with Whale. Total hits: " + hit);
+            lastCountTime = Time.time;
+        }
+
+        
+    }
+
+    
+}
---
+++ Assets/Scripts/Rules.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Rules : MonoBehaviour
+{
+    public TextMeshProUGUI count;
+
+    void Update()
+    {
+        count.text = "Ships Taken Down: " + ChildCollisionDetector.hit; 
+    }
+}
